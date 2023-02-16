7328d65ca73197b772df12517912ef11c1b0686c
dhrgodms <dhrgodms123@gmail.com>
2023-02-17 01:09:42 +0900

    230216-1002 터렛 너무 어렵다...

---
+++ 230216-1002.py
@@ -0,0 +1,12 @@
+import sys
+import math
+n = int(sys.stdin.readline().rstrip('\n'))
+case_list = list(list(map(int,sys.stdin.readline().rstrip('\n').split()))for _ in range(n))
+for i in case_list:
+    D = math.sqrt((i[3]-i[0])**2+(i[4]-i[1])**2)
+    if D==0 and i[2]==i[5]:
+        print(-1)
+        break
+    if D<i[2]+i[5] and D>abs(i[2]-i[5]):print(2)
+    elif D==i[2]+i[5] or D==abs(i[5]-i[2]):print(1)
+    else: print(0)
