a344f5d41bf9e422adc46de0912eee3a95fb4b92
dhrgodms <dhrgodms123@gmail.com>
2023-02-21 02:08:38 +0900

    230220....

--- 230220-10815.py
+++ 230220-10815.py
@@ -4,11 +4,21 @@ n = int(sys.stdin.readline().rstrip('\n'))
 n_list = list(map(int,sys.stdin.readline().rstrip('\n').split()))
 m = int(sys.stdin.readline().rstrip('\n'))
 m_list = list(map(int,sys.stdin.readline().rstrip('\n').split()))
-result_list = []
+
+n_list.sort()
+
 for i in m_list:
-    if i in n_list:
-        result_list.append(1)
-    else:
-        result_list.append(0)
-for i in result_list:
-    print(i, end=" ")
+    start,mid,end=0,(n-1)//2,n-1
+    while end-start>1:
+        if i < n_list[mid]:
+            end = mid
+            mid = end - (end-start)//2
+        elif i > n_list[mid]:
+            start = mid
+            mid = start+(end-start)//2
+        else:
+            print(1, end=" ")
+            break;
+    if end-start==1:
+        if n_list[start]!=i and n_list[end]!=i: print(0, end=" ")
+        else : print(1, end=" ")
